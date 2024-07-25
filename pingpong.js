14cccbe8d1bc7bbafb29bac4c57b8007c58ad30b
MatheusDaviBernardoCastro <168776463+MatheusDaviBernardoCastro@users.noreply.github.com>
2024-07-25 16:45:15 -0300

    .

--- pingpong.js
+++ pingpong.js
@@ -1,20 +1,22 @@
-// aqui eu vou criar a bolinha 
-let xbolinha = 300;
-let ybolinha =200;
-let tambolinhha = 25; 
+//aqui eu vou criar a bolinha
+let xBolinha = 300;
+let yBolinha = 200;
+let tamBolinha = 25;
+
 function setup(){
     //aqui vou criar minha "mesa"
     createCanvas(600,400);
 }
 
-function draw (){
+function draw(){
     //aqui vou por a cor da "mesa"
-    background(0,250,154);
-//chamndo função
-    criabolinha(xbolinha, ybolinha, tambolinhha)
+    //A cor da mesa é em rgb
+    background(128,128,0); 
+    //chamando a função
+    criaBolinha(xBolinha, yBolinha, tamBolinha);
 }
 
 //função bolinha
-function vria bolinha(xbolinha, ybolinha, tambolinhha){
-    CSSNumericValue(xbolinha, ybolinha, tambolinhha);
+function criaBolinha(xBolinha, yBolinha,tamBolinha){
+    circle (xBolinha,yBolinha,tamBolinha);
 }
